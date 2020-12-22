6834338250d2eb2b1a8e715349b31f8cdc6b7811
Hitohito1023 <ryotakinase1023@gmail.com>
2020-12-22 10:25:54 +0000

    [finish]comment_非同期化

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,12 +5,14 @@ class PostCommentsController < ApplicationController
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.post_item_id = @post_item.id
     @post_comment.user_id = current_user.id
+    @post_comments = @post_item.post_comments.order(created_at: :desc)
     @post_comment.save
     render :create
   end
 
   def destroy
     @post_item = PostItem.find(params[:post_item_id])
+    @post_comments = @post_item.post_comments.order(created_at: :desc)
     post_comment = @post_item.post_comments.find(params[:id])
     post_comment.destroy
     render :destroy
