f3ed017c72d7daa9e933bd103da16fdac1ceb08b
Hitohito1023 <ryotakinase1023@gmail.com>
2021-01-10 08:13:10 +0000

    [add]フォロワー一覧の追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,15 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings.page(params[:page])
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers.page(params[:page])
+  end
 
   def create
     current_user.follow(params[:user_id])
@@ -11,6 +20,5 @@ class RelationshipsController < ApplicationController
     current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
-
 
 end
